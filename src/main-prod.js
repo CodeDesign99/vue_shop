18199dadaa5e4f7ec9fc915fbfc9a355e9c49693
hwx0753 <75604065@qq.com>
2021-04-17 01:17:48 +0800

    完结

--- src/main-prod.js
+++ src/main-prod.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './plugins/element.js'
+// import './plugins/element.js'
 //导入全局样式
 import './assets/css/global.css'
 //导入字体图标
@@ -12,16 +12,30 @@ import TreeTable from 'vue-table-with-tree-grid'
 //导入富文本编辑器
 import VueQuillEditor from 'vue-quill-editor'
 //require style 导入富文本编辑器对应的样式 
-import 'quill/dist/quill.core.css'
-import 'quill/dist/quill.snow.css'
-import 'quill/dist/quill.bubble.css'
+// import 'quill/dist/quill.core.css'
+// import 'quill/dist/quill.snow.css'
+// import 'quill/dist/quill.bubble.css'
+
+// 导入 NProgress 包对应的JS和CSS
+import NProgress from 'nprogress'
+// import 'nprogress/nprogress.css'
 
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+
+//在 request 拦截器中，展示进度条 NProgress.start()
 axios.interceptors.request.use(config=>{
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
   return config
 })
+
+// 在response拦截器中隐藏进度条  NProgress.done()
+axios.interceptors.response.use(config=>{
+  NProgress.done()
+  return config
+})
+
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
